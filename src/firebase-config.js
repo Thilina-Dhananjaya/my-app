29371cbc3dffa5d5ed889f9498c59b8ff10e1b5d
Thilina-Dhananjaya <120640837+Thilina-Dhananjaya@users.noreply.github.com>
2022-12-23 14:25:51 +0530

    Update firebase-config.js
    Removed sensitive data

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBmYVPlkSCoSBSohRW200jchnnQGTei1a4",
-  authDomain: "authentication-19232.firebaseapp.com",
-  projectId: "authentication-19232",
-  storageBucket: "authentication-19232.appspot.com",
-  messagingSenderId: "407649153616",
-  appId: "1:407649153616:web:f91d2dd6a8bc724f999755",
-  measurementId: "G-FL948FL04J"
+const firebaseConfig = {        //"private" = Removed bcs of sensitivity
+  apiKey: "private",
+  authDomain: "private",
+  projectId: "private",
+  storageBucket: "private",
+  messagingSenderId: "private",
+  appId: "private",
+  measurementId: "private"
 };
 
 const app = initializeApp(firebaseConfig); 
